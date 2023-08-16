7345ca3382984593133590b6f20954d5f38ac04c
ombhojane <ombhojane05@gmail.com>
2023-08-16 23:37:57 +0530

    ui improvements in welcomepass

--- lib/screens/ads_view.dart
+++ lib/screens/ads_view.dart
@@ -1,27 +1,13 @@
-import 'package:fluentui_icons/fluentui_icons.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../utils/app_styles.dart';
-import 'package:gap/gap.dart';
-import 'dart:math' as math;
 
-class Ads extends StatelessWidget {
-  const Ads({super.key});
-
+class BusCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        // Navigate to the "Welcome Pass" page
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => WelcomePassPage(),
-        ));
-      },
-      child: SizedBox(
-      width: MediaQuery.of(context).size.width*0.85,
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.85,
       height: 210,
       child: Container(
-        // padding: const EdgeInsets.only(top: 30),
         margin: const EdgeInsets.only(right: 15, top: 30),
         child: Column(
           children: [
@@ -29,7 +15,12 @@ class Ads extends StatelessWidget {
               height: 170,
               decoration: const BoxDecoration(
                 color: Color(0xFF6C63FF),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomLeft: Radius.circular(20), topRight: Radius.circular(20), bottomRight: Radius.circular(20)),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
               ),
               padding: const EdgeInsets.only(right: 16, top: 16, bottom: 16),
               child: Row(
@@ -40,21 +31,16 @@ class Ads extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                        // padding: const EdgeInsets.only( right: 10),
                         child: SizedBox(
                           width: 135,
                           height: 135,
                           child: Container(
                             decoration: BoxDecoration(
-                              // color: Colors.white,
                               borderRadius: BorderRadius.circular(10),
                             ),
-                            child: const Image(image: AssetImage('lib/assets/loveit_heart.png'),),
-                            // child: Column(
-                            //   children: [
-                            //     // Text("h")
-                            //   ],
-                            // ),
+                            child: const Image(
+                              image: AssetImage('lib/assets/loveit_heart.png'),
+                            ),
                           ),
                         ),
                       )
@@ -66,22 +52,38 @@ class Ads extends StatelessWidget {
                     children: [
                       Row(
                         children: [
-                          Text("BUS PASS", style: Styles.headlineStyle2.copyWith(color: Colors.white, fontSize: 27),)
+                          Text(
+                            "BUS PASS",
+                            style: Styles.headlineStyle2.copyWith(
+                                color: Colors.white, fontSize: 27),
+                          )
                         ],
                       ),
                       Row(
                         children: [
-                          Text("starting at", style: Styles.headlineStyle4.copyWith(color: Colors.white),)
+                          Text(
+                            "starting at",
+                            style: Styles.headlineStyle4.copyWith(
+                                color: Colors.white),
+                          )
                         ],
                       ),
                       Row(
                         children: [
-                          Text("₹9", style: Styles.headlineStyle1.copyWith(color: Colors.white),)
+                          Text(
+                            "₹9",
+                            style: Styles.headlineStyle1.copyWith(
+                                color: Colors.white),
+                          )
                         ],
                       ),
                       Row(
                         children: [
-                          Text("Terms and Conditions Applyᵀᴹ.", style: Styles.headlineStyle4.copyWith(color: Colors.white, fontSize: 5),)
+                          Text(
+                            "Terms and Conditions Applyᵀᴹ.",
+                            style: Styles.headlineStyle4.copyWith(
+                                color: Colors.white, fontSize: 5),
+                          )
                         ],
                       ),
                     ],
@@ -92,13 +94,25 @@ class Ads extends StatelessWidget {
           ],
         ),
       ),
-    ),
-    
-  
-    );}
+    );
+  }
 }
 
+class Ads extends StatelessWidget {
+  const Ads({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => WelcomePassPage(),
+        ));
+      },
+      child: BusCard(),
+    );
+  }
+}
 
 class WelcomePassPage extends StatelessWidget {
   @override
@@ -112,14 +126,10 @@ class WelcomePassPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 30),
-            Text(
-              "Welcome Pass",
-              style: Styles.headlineStyle2.copyWith(color: Styles.primaryColor),
-            ),
+            Center(child: BusCard()),
             SizedBox(height: 20),
             Text(
-              "Get Welcome Pass at just ₹9 and enjoy 5 bus trips up to ₹25 each.",
+              "Introducing the Welcome Pass for just ₹9! Enjoy 5 bus trips, each giving you a discount of up to ₹25..",
               style: Styles.textStyle,
             ),
             SizedBox(height: 30),
