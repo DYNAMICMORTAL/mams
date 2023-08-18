0525a9a6c49808f265fd0bf5d6520b78cfa0ec95
Mihir <aminvasudev6@gmail.com>
2023-08-18 22:52:34 +0530

    fixed bug

--- lib/inkwell_pages/123Down_view.dart
+++ lib/inkwell_pages/123Down_view.dart
@@ -95,6 +95,175 @@
 
 
 
+//
+// import 'package:fluentui_icons/fluentui_icons.dart';
+// import 'package:flutter/material.dart';
+// import 'package:gap/gap.dart';
+// import '../maps/map_overview.dart';
+// import '../utils/app_styles.dart';
+//
+// class Down123 extends StatefulWidget {
+//   const Down123({Key? key}) : super(key: key);
+//
+//   @override
+//   _Down123State createState() => _Down123State();
+// }
+//
+// class _Down123State extends State<Down123> {
+//   double _containerHeight = 0.5;
+//   bool _showDepotContainer = false;
+//   bool _showRailwayStationContainer = false;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           Column(
+//             children: [
+//               Expanded(
+//                 child: MapsOverview(),
+//               ),
+//               GestureDetector(
+//                 onVerticalDragUpdate: (details) {
+//                   setState(() {
+//                     _containerHeight -= details.delta.dy / MediaQuery.of(context).size.height;
+//                     _containerHeight = _containerHeight.clamp(0.25, 0.9);
+//                   });
+//                 },
+//                 child: Container(
+//                   height: MediaQuery.of(context).size.height * _containerHeight,
+//                   padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     borderRadius: BorderRadius.only(topRight: Radius.circular(25), topLeft: Radius.circular(25)),
+//                   ),
+//                   child: Column(
+//                     children: [
+//                       Row(
+//                         children: [
+//                           Expanded(
+//                             child: Text(
+//                               "123 - Mulund Depot -> Kasarvadawli",
+//                               style: Styles.headlineStyle2.copyWith(fontSize: 18),
+//                               overflow: TextOverflow.ellipsis,
+//                             ),
+//                           ),
+//                           const Gap(10),
+//                           ElevatedButton(
+//                             onPressed: () {
+//                               Navigator.pushNamed(context, '/chat'); // Replace with your route
+//                             },
+//                             style: ElevatedButton.styleFrom(
+//                               backgroundColor: Colors.black,
+//                               shape: RoundedRectangleBorder(
+//                                 borderRadius: BorderRadius.circular(8),
+//                               ),
+//                             ),
+//                             child: Text(
+//                               "Chat",
+//                               style: TextStyle(color: Colors.white),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Container(
+//                             child: Column(
+//                               children: [
+//                                 GestureDetector(
+//                                   onTap: () {
+//                                     setState(() {
+//                                       _showRailwayStationContainer = !_showRailwayStationContainer;
+//                                     });
+//                                   },
+//                                   child: Align(
+//                                     alignment: Alignment.centerLeft,
+//                                     child: Text(
+//                                       "Mulund Depot", style: Styles.headlineStyle3,
+//                                     ),
+//                                   ),
+//                                 ),
+//                                 Visibility(
+//                                   visible: _showRailwayStationContainer,
+//                                   child: Container(
+//                                     child: Column(
+//                                       crossAxisAlignment: CrossAxisAlignment.start,
+//                                       children: [
+//                                         Text("Arriving in"),
+//                                         Row(
+//                                           children: [
+//                                             Icon(FluentSystemIcons.ic_fluent_wifi_1_filled),
+//                                             const Spacer(),
+//                                             Text("2 Minutes"),
+//                                           ],
+//                                         ),
+//                                       ],
+//                                     ),
+//                                   ),
+//                                 ),
+//                               ],
+//                             ),
+//                           ),
+//                           Container(
+//                             child: Column(
+//                               crossAxisAlignment: CrossAxisAlignment.start,
+//                               children: [
+//                                 GestureDetector(
+//                                   onTap: () {
+//                                     setState(() {
+//                                       _showDepotContainer = !_showDepotContainer;
+//                                     });
+//                                   },
+//                                   child: Text(
+//                                     "Mulund Railway Station West", style: Styles.headlineStyle3,
+//                                   ),
+//                                 ),
+//                                 Visibility(
+//                                   visible: _showDepotContainer,
+//                                   child: Container(
+//                                     child: Column(
+//                                       crossAxisAlignment: CrossAxisAlignment.start,
+//                                       children: [
+//                                         Text("Arriving in"),
+//                                         Row(
+//                                           children: [
+//                                             Icon(FluentSystemIcons.ic_fluent_wifi_1_filled),
+//                                             const Spacer(),
+//                                             Text("2 Minutes"),
+//                                           ],
+//                                         ),
+//                                       ],
+//                                     ),
+//                                   ),
+//                                 ),
+//                               ],
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+//
+
+
+
+
+
+
+
 
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
@@ -113,6 +282,20 @@ class _Down123State extends State<Down123> {
   double _containerHeight = 0.5;
   bool _showDepotContainer = false;
   bool _showRailwayStationContainer = false;
+
+  void _toggleDepotContainer() {
+    setState(() {
+      _showDepotContainer = !_showDepotContainer;
+      _showRailwayStationContainer = false; // Close the other container
+    });
+  }
+
+  void _toggleRailwayStationContainer() {
+    setState(() {
+      _showRailwayStationContainer = !_showRailwayStationContainer;
+      _showDepotContainer = false; // Close the other container
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -174,11 +357,7 @@ class _Down123State extends State<Down123> {
                             child: Column(
                               children: [
                                 GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      _showRailwayStationContainer = !_showRailwayStationContainer;
-                                    });
-                                  },
+                                  onTap: _toggleDepotContainer,
                                   child: Align(
                                     alignment: Alignment.centerLeft,
                                     child: Text(
@@ -187,7 +366,7 @@ class _Down123State extends State<Down123> {
                                   ),
                                 ),
                                 Visibility(
-                                  visible: _showRailwayStationContainer,
+                                  visible: _showDepotContainer,
                                   child: Container(
                                     child: Column(
                                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -212,17 +391,13 @@ class _Down123State extends State<Down123> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      _showDepotContainer = !_showDepotContainer;
-                                    });
-                                  },
+                                  onTap: _toggleRailwayStationContainer,
                                   child: Text(
                                     "Mulund Railway Station West", style: Styles.headlineStyle3,
                                   ),
                                 ),
                                 Visibility(
-                                  visible: _showDepotContainer,
+                                  visible: _showRailwayStationContainer,
                                   child: Container(
                                     child: Column(
                                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -255,5 +430,3 @@ class _Down123State extends State<Down123> {
     );
   }
 }
-
-
