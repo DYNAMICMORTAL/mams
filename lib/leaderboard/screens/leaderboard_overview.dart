b91f3aa98067c558130812d72c9381d2e169e993
Mihir <aminvasudev6@gmail.com>
2023-08-30 00:47:50 +0530

    UXFix1.1.0

--- lib/leaderboard/screens/leaderboard_overview.dart
+++ lib/leaderboard/screens/leaderboard_overview.dart
@@ -1,9 +1,100 @@
-import 'package:booktickets/utils/app_styles.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:booktickets/utils/app_styles.dart';
+//
+// class LeaderOverView extends StatefulWidget {
+//   const LeaderOverView({Key? key}) : super(key: key);
+//
+//   @override
+//   _LeaderOverViewState createState() => _LeaderOverViewState();
+// }
+//
+// class _LeaderOverViewState extends State<LeaderOverView> {
+//   bool isImageOverText = false;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Container(
+//           padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text("LEADERBOARD", style: Styles.headlineStyle1),
+//               SizedBox(height: 20),
+//               Stack(
+//                 alignment: Alignment.centerLeft, // Align text to the left
+//                 children: [
+//                   Container(
+//                     height: 240, // Adjust the height as needed
+//                     child: NotificationListener<ScrollNotification>(
+//                       onNotification: (notification) {
+//                         if (notification is ScrollUpdateNotification) {
+//                           setState(() {
+//                             isImageOverText =
+//                                 notification.metrics.pixels > 0; // Check if images are over text
+//                           });
+//                         }
+//                         return false;
+//                       },
+//                       child: ListView.builder(
+//                         scrollDirection: Axis.horizontal,
+//                         itemCount: 4, // Add more as needed
+//                         itemBuilder: (context, index) {
+//                           return ClipRRect(
+//                             borderRadius: BorderRadius.circular(45),
+//                             child: Container(
+//                               margin: EdgeInsets.all(8),
+//                               width: 175, // Adjust the width as needed
+//                               color: Colors.blueGrey,
+//                               // Your image and user info widgets here
+//                             ),
+//                           );
+//                         },
+//                       ),
+//                     ),
+//                   ),
+//                   AnimatedOpacity(
+//                     opacity: isImageOverText ? 0.0 : 1.0,
+//                     duration: Duration(milliseconds: 400),
+//                     child: Text(
+//                       "Top 20",
+//                       style: TextStyle(
+//                         fontSize: 60,
+//                         color: Colors.black,
+//                         fontWeight: FontWeight.w900,
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class LeaderOverView extends StatelessWidget {
-  const LeaderOverView({super.key});
+
+
+
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:booktickets/utils/app_styles.dart';
+
+class LeaderOverView extends StatefulWidget {
+  const LeaderOverView({Key? key}) : super(key: key);
+
+  @override
+  _LeaderOverViewState createState() => _LeaderOverViewState();
+}
+
+class _LeaderOverViewState extends State<LeaderOverView> {
+  bool isImageOverText = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,28 +103,136 @@ class LeaderOverView extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width*0.9,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Column(
-                        children: [
-                          Text("LEADERBOARD",style: Styles.headlineStyle1,),
-                        ],
+              Text("LEADERBOARD", style: Styles.headlineStyle1),
+              SizedBox(height: 20),
+              Stack(
+                alignment: Alignment.centerLeft, // Align text to the left
+                children: [
+                  Container(
+                    height: 240, // Adjust the height as needed
+                    child: NotificationListener<ScrollNotification>(
+                      onNotification: (notification) {
+                        if (notification is ScrollUpdateNotification) {
+                          setState(() {
+                            isImageOverText =
+                                notification.metrics.pixels > 0; // Check if images are over text
+                          });
+                        }
+                        return false;
+                      },
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 4, // Add more as needed
+                        itemBuilder: (context, index) {
+                          // Edit container 1
+                          if (index == 0) {
+                            return Container(
+                              margin: EdgeInsets.all(8),
+                              width: 175, // Adjust the width as needed
+                              decoration: BoxDecoration(
+                                // color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(25),
+                                image: DecorationImage(
+                                  image: AssetImage('lib/assets/girl1.jpg'), // Replace with your image path
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              child: Container(
+                                padding: const EdgeInsets.only(left: 20, bottom: 10),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("#1",style: Styles.headlineStyle2.copyWith(fontSize: 25, fontWeight: FontWeight.w900, color: Colors.white), ),
+                                    Text("Mansi Ja..",style: Styles.headlineStyle5.copyWith(color: Colors.orange, fontWeight: FontWeight.w900, fontSize: 28,),),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }
+                          if (index == 1) {
+                            return Container(
+                              margin: EdgeInsets.all(8),
+                              width: 175, // Adjust the width as needed
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Container 2",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                          if (index == 2) {
+                            return Container(
+                              margin: EdgeInsets.all(8),
+                              width: 175, // Adjust the width as needed
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Container 3",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                          if (index == 3) {
+                            return Container(
+                              margin: EdgeInsets.all(8),
+                              width: 175, // Adjust the width as needed
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Container 4",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                          // Add code for other containers here
+                          return Container(
+                            margin: EdgeInsets.all(8),
+                            width: 175, // Adjust the width as needed
+                            color: Colors.blueGrey,
+                            // Your image and user info widgets here
+                          );
+                        },
                       ),
                     ),
-                    Container(
-                      child: Column(
-                        children: [
-
-                        ],
+                  ),
+                  AnimatedOpacity(
+                    opacity: isImageOverText ? 0.0 : 1.0,
+                    duration: Duration(milliseconds: 400),
+                    child: Text(
+                      "Top 20",
+                      style: TextStyle(
+                        fontSize: 60,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w900,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
