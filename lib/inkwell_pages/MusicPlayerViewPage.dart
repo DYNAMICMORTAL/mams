e2fa76d38fe66e4c6ea6f8647df6875bb6b36d28
Mihir <aminvasudev6@gmail.com>
2023-10-24 17:04:38 +0530

    MusicBuildv0.1.0

---
+++ lib/inkwell_pages/MusicPlayerViewPage.dart
@@ -0,0 +1,105 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:video_player/video_player.dart';
+
+class MusicPlayerViewPage extends StatefulWidget {
+  const MusicPlayerViewPage({Key? key}) : super(key: key);
+
+  @override
+  _MusicPlayerViewPageState createState() => _MusicPlayerViewPageState();
+}
+
+class _MusicPlayerViewPageState extends State<MusicPlayerViewPage> {
+  late AudioPlayer advancedPlayer;
+  late VideoPlayerController _videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    advancedPlayer = AudioPlayer();
+    _videoPlayerController = VideoPlayerController.asset('lib/assets/MusicGif1.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized
+        setState(() {});
+
+        // Start video playback
+        _videoPlayerController.play();
+
+        // Set up a listener to detect when the video playback is complete
+        _videoPlayerController.addListener(() {
+          if (_videoPlayerController.value.position >= _videoPlayerController.value.duration) {
+            setState(() {
+              // Video playback is complete
+            });
+          }
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            if (_videoPlayerController.value.isInitialized)
+              AspectRatio(
+                aspectRatio: _videoPlayerController.value.aspectRatio,
+                child: VideoPlayer(_videoPlayerController),
+              ),
+            if (_videoPlayerController.value.isInitialized &&
+                _videoPlayerController.value.position < _videoPlayerController.value.duration)
+              Container(), // Video is playing, display nothing
+            if (_videoPlayerController.value.position >= _videoPlayerController.value.duration)
+              Container(
+                // margin: const EdgeInsets.only(left: 15, top: 12),
+                // child: Column(
+                //   children: [
+                //     Row(
+                //       crossAxisAlignment: CrossAxisAlignment.center,
+                //       children: [
+                //         Container(
+                //           child: Transform.rotate(
+                //             angle: 90 * (3.1415926535 / 180),
+                //             child: Icon(FluentSystemIcons.ic_fluent_ios_chevron_right_regular, size: 30),
+                //           ),
+                //         ),
+                //         const Gap(65),
+                //         Container(
+                //           child: Column(
+                //             children: [
+                //               Text("Playing from Album", style: TextStyle(color: Colors.white)),
+                //               Text("From Heart to Heart", style: TextStyle(color: Colors.white)),
+                //             ],
+                //           ),
+                //         )
+                //       ],
+                //     ), // Music Album Info and Back Button
+                //     Container(), // Main Music Container
+                //     Container(), // Audio Player Container
+                //     Container(), // Lyrics Container
+                //   ],
+                // ),
+              ), // MAIN CONTAINER
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MusicPlayerViewPage(),
+    ),
+  );
+}
--- lib/screens/MusicOverViewPage.dart
+++ lib/screens/MusicOverViewPage.dart
@@ -1,6 +1,12 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:mams/audios/audio_file.dart';
+import 'package:mams/screens/profile_view.dart';
 import 'package:mams/utils/app_styles.dart';
+
+import '../inkwell_pages/MusicPlayerViewPage.dart';
 
 class MusicOverView extends StatelessWidget {
   const MusicOverView({super.key});
@@ -20,23 +26,86 @@ class MusicOverView extends StatelessWidget {
                     children: [
                       Text("Listen Now", style: Styles.headlineStyle1,),
               const Spacer(),
-              Container(
-                width: 49.5, // Adjust the size of the circle
-                height: 49.5, // Adjust the size of the circle
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.blue, // Change the color of the circle
-                ),
-                child: Center(
-                  child: Icon(
-                    Icons.star,
-                    color: Colors.white, // Change the color of the icon
-                    size: 20.0, // Adjust the size of the icon
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfileView(),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: 49.5, // Adjust the size of the circle
+                  height: 49.5, // Adjust the size of the circle
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue, // Change the color of the circle
                   ),
+                  child: Center(
+                    child: Icon(
+                        Icons.star,
+                        color: Colors.white, // Change the color of the icon
+                        size: 20.0, // Adjust the size of the icon
+                      ),
+                    ),
                 ),
               ),
                     ],
                   ),//NavBar MAMS TUNES
+                  const Gap(10),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MusicPlayerViewPage(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 15, top: 18, bottom: 18),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 32, // Adjust the size of the circle
+                                  height:32, // Adjust the size of the circle
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.rectangle,
+                                    color: Colors.blue, // Change the color of the circle
+                                  ),
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.star,
+                                      color: Colors.white, // Change the color of the icon
+                                      size: 20.0, // Adjust the size of the icon
+                                    ),
+                                  ),
+                                ),
+                                const Gap(15),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("NORO", style: Styles.headlineStyle3.copyWith(fontSize: 21),),
+                                    Text("?Shimon", style: Styles.headlineStyle4.copyWith(fontSize: 10, color: Colors.grey.shade700),),
+                                  ],
+                                ),
+                                const Spacer(),
+                                Icon(FluentSystemIcons.ic_fluent_arrow_right_filled, size: 30,),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),//First Music Audio
                 ],
               ),
             ),
