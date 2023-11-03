e38d0200f2397cd813c3cf06fd40145ddd597831
Mihir <aminvasudev6@gmail.com>
2023-11-03 06:40:03 +0530

    MMRDA Page MMRDA line 1

--- lib/inkwell_pages/MMRDA1_view.dart
+++ lib/inkwell_pages/MMRDA1_view.dart
@@ -86,7 +86,118 @@ class MMRDA1 extends StatelessWidget {
                       ],
                     ),
                   ),
+                  const Gap(35),
+                  Container(
+                    padding: const EdgeInsets.only(left: 5, right:5),
+                    width: MediaQuery.of(context).size.width*0.90,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("Choose an option to Travel", style: Styles.headlineStyle3),
+                        const Gap(12),
+                        Container(
+                          padding: const EdgeInsets.only(left: 15, right: 15, top: 8, bottom: 8),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(FluentSystemIcons.ic_fluent_phone_mobile_regular),
+                                        const Gap(15),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("Metro QR Ticket"),
+                                              const Gap(1),
+                                              Text(
+                                                "Digital tickets for on-the-go journey. We believe in Paperless and Eco-Friendly ticketing systems",
+                                                style: Styles.headlineStyle4.copyWith(overflow: TextOverflow.ellipsis),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Icon(FluentSystemIcons.ic_fluent_chevron_right_regular),
+                                      ],
+                                    ),
+                                    // Add more rows as needed...
+                                  ],
+                                ),
+                              ),
+                              const Gap(20),
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(FluentSystemIcons.ic_fluent_contact_card_regular),
+                                        const Gap(15),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("MAMS Metro Pass"),
+                                              const Gap(1),
+                                              Text(
+                                                "Multiple trips in one card. Recharge any time and use anywhere.",
+                                                style: Styles.headlineStyle4.copyWith(overflow: TextOverflow.ellipsis),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Icon(FluentSystemIcons.ic_fluent_chevron_right_regular),
+                                      ],
+                                    ),
+                                    // Add more rows as needed...
+                                  ],
+                                ),
+                              ),
+                              const Gap(20),
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(FluentSystemIcons.ic_fluent_contact_card_group_regular),
+                                        const Gap(15),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("Multi QR Ticket"),
+                                              const Gap(1),
+                                              Text(
+                                                "Mutiple trip QR for any amount, use immediately at station",
+                                                style: Styles.headlineStyle4.copyWith(overflow: TextOverflow.ellipsis),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Icon(FluentSystemIcons.ic_fluent_chevron_right_regular),
+                                      ],
+                                    ),
+                                    // Add more rows as needed...
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
+
               ),
             ),
           ],
